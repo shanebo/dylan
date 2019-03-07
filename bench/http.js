87d06503fbab97bf5cdb4406f263922ea2567578
Shane Thacker <shane@steadymade.com>
2019-03-07 11:32:56 -0600

    Add more routes to bench apps

--- bench/http.js
+++ bench/http.js
@@ -2,8 +2,10 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3100;
 const server = http.createServer((req, res) => {
-  if (req.url === '/favicon.ico') return;
-  if (req.url === '/') return res.end('Hello');
+  req.one = true;
+  req.two = true;
+  if (req.url === '/favicon.ico') return res.end('favicon.ico');
+  if (req.url === '/') return res.end('hello world');
   if (req.url === '/user/123') return res.end('User 123');
 });
 
